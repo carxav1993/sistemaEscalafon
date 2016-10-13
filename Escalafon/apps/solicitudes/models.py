4d3b08b4352e52c3fc8c3ce11897c5960a2ef17d
Carlos Vega <cvcarlosvega93@gmail.com>
2016-10-13 16:25:19 -0500

    a

--- Escalafon/apps/solicitudes/models.py
+++ Escalafon/apps/solicitudes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class inscripcion(models.model):
+	cedula = models.Charfield(max_length=10)
+	apellidoNombre = models.Charfield(max_length=150)
+	
